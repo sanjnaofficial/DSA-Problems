2cc0547c2a49ca033cbb5d7dfdfd236bb6a67ddf
Sanjna <sanjna.contact@gmail.com>
2024-07-18 10:31:42 +0530

    Dynamic programing programs

---
+++ Dynamic Programming/FrogJump.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// frog can jump once or twice. we need to reach the nth step with minumum energy. Energy= |difference between jth stair - ith stair|
+public class FrogJump {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+
+    static void minEnergy(int n){// 
+
+    }
+}
